1d38ecb94eb4f12dcea5fa0e555b61bce53aa3c6
Joseph Diekhoff <jdiekhoff@rapidbitdev.com>
2012-01-26 22:18:44 -0500

    Adds RateView to project.

---
+++ RateView.h
@@ -0,0 +1,42 @@
+//
+//  RateView.h
+//  ScaryBugs
+//
+//  Created by Joseph Diekhoff on 1/26/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RateView;
+
+@protocol RateViewDelegate
+- (void)rateView:(RateView *)rateView ratingDidChange:(float)rating;
+
+@end
+
+@interface RateView : UIView {
+	UIImage *_notSelectedImage;
+	UIImage *_halfSelectedImage;
+	UIImage *_fullSelectedImage;
+	float _rating;
+	BOOL _editable;
+	NSMutableArray *_imageViews;
+	int _maxRating;
+	int _midMargin;
+	int _leftMargin;
+	CGSize _minImageSize;
+	id<RateViewDelegate> _delegate;
+}
+
+@property (retain) UIImage *notSelectedImage;
+@property (retain) UIImage *halfSelectedImage;
+@property (retain) UIImage *fullSelectedImage;
+@property float rating;
+@property BOOL editable;
+@property int maxRating;
+@property (assign) id<RateViewDelegate> delegate;
+@property int leftMargin;
+
+
+@end
